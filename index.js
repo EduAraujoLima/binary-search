73efebc3acbb3734398109fd23c12a7f27281689
Eduardo Araújo <edu.wp@outlook.com>
2022-02-11 22:08:34 -0300

    Loop completo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function BinarySearch(min, max, key, attempt = 1) {
     const mid = Math.floor((min + max) / 2);
 
-    if(mid === key) return `${key} found in ${mid}`;
+    if(mid === key) return attempt;
 
     if(key < mid) {
         console.log(`Attempt ${attempt} - ${key} is less than ${mid}`);
@@ -15,11 +15,20 @@ function BinarySearch(min, max, key, attempt = 1) {
 
 //BinarySearch(1, 100, 45)
 
-function randomNumber(max = 1000, min = 0) {
-    for(let i = 0; i < 1000; i++ ) {
+let maxnumber = 100
+
+function randomNumber(max = maxnumber, min = 1) {
+    let maxAttemp = 0;
+    for(let i = 0; i < maxnumber; i++ ) {
         const rdn = Math.floor(Math.random() * (max - min) + min);
-        BinarySearch(min, max, rdn)
+        const attempt = BinarySearch(min, max, rdn)
+        if(!isNaN(attempt)) {
+            maxAttemp = maxAttemp < attempt ? attempt : maxAttemp
+        }
+        console.log('-------------------------------')
     }
+
+    console.log("MaxAttemp", maxAttemp)
 }
 
 randomNumber()
