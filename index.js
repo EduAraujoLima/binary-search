aff935fdfb271e8284c8407dea4bda6652ad1f78
Eduardo Araújo <edu.wp@outlook.com>
2022-02-11 22:22:49 -0300

    Correct attempt log

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function BinarySearch(min, max, key, attempt = 1) {
     const mid = Math.floor((min + max) / 2);
 
-    if(mid === key) return attempt;
+    if(mid === key) {
+        console.log(`Attempt ${attempt} - ${key} is matched`);
+        return attempt
+    };
 
     if(key < mid) {
-        console.log(`Attempt ${attempt} - ${key} is less than ${mid}`);
+       console.log(`Attempt ${attempt} - ${key} is less than ${mid}`);
         return BinarySearch(min, mid - 1, key, attempt + 1);
     } else {
         console.log(`Attempt ${attempt} - ${key} is higher than ${mid}`);
@@ -12,14 +15,12 @@ function BinarySearch(min, max, key, attempt = 1) {
     }
 }
 
-
-//BinarySearch(1, 100, 45)
-
-let maxnumber = 100
+let maxnumber = 1000000
 
 function randomNumber(max = maxnumber, min = 1) {
-    let maxAttemp = 0;
-    for(let i = 0; i < maxnumber; i++ ) {
+    let maxAttemp = -1;
+
+    for(let i = 0; i < 10; i++ ) {
         const rdn = Math.floor(Math.random() * (max - min) + min);
         const attempt = BinarySearch(min, max, rdn)
         if(!isNaN(attempt)) {
